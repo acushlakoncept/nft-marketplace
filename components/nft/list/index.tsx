93b2e3a74ab7f613c2ea031e307dd727559fcd10
Uduak Essien <acushla4real@gmail.com>
2022-05-06 16:43:19 +0100

    Add Nft list

---
+++ components/nft/list/index.tsx
@@ -0,0 +1,21 @@
+import { FunctionComponent } from "react";
+import NftItem from "../item";
+
+type NftListProps = {
+  nfts: any[];
+}
+
+
+const NftList: FunctionComponent<NftListProps> = ({nfts}) => {
+  return (
+    <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+      { nfts.map((nft, index) => (
+        <div key={index} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+          <NftItem />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default NftList
