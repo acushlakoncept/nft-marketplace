f8c65117dd11130bc41829cd6c9cf985b47df55e
Uduak Essien <acushla4real@gmail.com>
2022-05-06 15:32:23 +0100

    Add baselayout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Navbar } from '../components'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Navbar />
       <Component {...pageProps} />
     </>
   )
