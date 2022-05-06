b19df978499b1c3fca508b5c6d3995bd7e5fd0b5
Uduak Essien <acushla4real@gmail.com>
2022-05-06 16:44:34 +0100

    Add default exports for Nft list and item

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,6 @@
 
 
 export { default as Navbar } from './navbar';
+export { default as BaseLayout } from './layout/BaseLayout'
+export { default as NftList } from './nft/list';
+export { default as NftItem } from './nft/item';
