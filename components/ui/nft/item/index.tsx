692f59493fd82a55a33a9ab1b01455893e1b48cd
Uduak Essien <acushla4real@gmail.com>
2022-05-06 17:44:59 +0100

    Update paths and folder structure

--- components/ui/nft/item/index.tsx
+++ components/ui/nft/item/index.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent } from "react";
-import { NftMeta } from "../../../types/nft";
+import { NftMeta } from "../../../../types/nft";
 
 type NftItemProps = {
   item: NftMeta;
