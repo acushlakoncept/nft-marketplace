1674aee34ba2bdbf3f4122b55a895fea9ddf09a7
Uduak Essien <acushla4real@gmail.com>
2022-05-06 17:15:34 +0100

    Add profile page

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -89,34 +89,16 @@ export default function Navbar() {
                     <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10">
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Your Profile
-                          </a>
+                          <Link href="/profile">
+                            <a
+                              className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            >
+                              Your Profile
+                            </a>
+                          </Link>
                         )}
                       </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
+                      
                     </Menu.Items>
                   </Transition>
                 </Menu>
---
+++ pages/profile.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next";
+import { BaseLayout } from "../components";
+
+
+
+const Profile: NextPage = () => {
+  return (
+    <BaseLayout>
+      <h1>Profile Page</h1>
+    </BaseLayout>
+  )
+}
+
+export default Profile;
