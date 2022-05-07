3cb520c9e159e68c0b68efb29561625b3623486a
Uduak Essien <acushla4real@gmail.com>
2022-05-07 13:06:09 +0100

    Add web3Provider

---
+++ components/providers/web3/index.tsx
@@ -0,0 +1,20 @@
+import { createContext, FunctionComponent, useContext, useState } from "react";
+
+
+const Web3Context = createContext<any>(null);
+
+const Web3Provider: FunctionComponent = ({children}) => {
+  const [web3Api, setWeb3Api] = useState({test: "Hello provider"});
+
+  return (
+    <Web3Context.Provider value={web3Api}>
+      {children}
+    </Web3Context.Provider>
+  )
+}
+
+export function useWeb3() {
+  return useContext(Web3Context);
+}
+
+export default Web3Provider;
