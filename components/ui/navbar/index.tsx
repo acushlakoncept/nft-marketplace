a4ce244e0aec54d132e9c17f6688eac4ed922d70
Uduak Essien <acushla4real@gmail.com>
2022-05-08 19:43:40 +0100

    Refactor setupHooks

--- components/ui/navbar/index.tsx
+++ components/ui/navbar/index.tsx
@@ -4,6 +4,7 @@ import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 import Link from 'next/link'
 import ActiveLink from '../link'
 import { useWeb3 } from '@providers/web3'
+import { useAccount } from '@hooks/web3'
 
 const navigation = [
   { name: 'Marketplace', href: '/', current: true },
@@ -16,10 +17,9 @@ function classNames(...classes: string[]) {
 
 export default function Navbar() {
 
-  const { hooks } = useWeb3();
-  const { data } = hooks.useAccount("account");
+  const {account} = useAccount();
 
-  console.log(data);
+  console.log(account.data);
 
   return (
     <Disclosure as="nav" className="bg-gray-800">
