8ece3f78971733d132892db1b79b9fd44391327e
Uduak Essien <acushla4real@gmail.com>
2022-05-09 04:46:27 +0100

    Add walletbar component

--- components/ui/navbar/index.tsx
+++ components/ui/navbar/index.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import ActiveLink from '../link'
 import { useWeb3 } from '@providers/web3'
 import { useAccount } from '@hooks/web3'
+import Walletbar from './walletbar'
 
 const navigation = [
   { name: 'Marketplace', href: '/', current: true },
@@ -67,63 +68,13 @@ export default function Navbar() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-
-                {/* Profile dropdown */}
-                { false ?
-
-                <Menu as="div" className="ml-3 relative">
-                  <div>
-                    <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link href="/profile">
-                            <a
-                              className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                            >
-                              Your Profile
-                            </a>
-                          </Link>
-                        )}
-                      </Menu.Item>
-                      
-                    </Menu.Items>
-                  </Transition>
-                </Menu> :
-                <button
-                  onClick={() => {
-                    account.connect();
-                  }}
-                  type="button"
-                  className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                  Connect Wallet
-                </button>
-                }
+                
+                <Walletbar 
+                  isLoading={account.isLoading}
+                  isInstalled={account.isInstalled}
+                  connect={account.connect}
+                  account={account.data}
+                 />
 
               </div>
             </div>
