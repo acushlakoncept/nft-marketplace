3aeac172b3594a50dbe32a5a544cb427a622d102
Uduak Essien <acushla4real@gmail.com>
2022-05-10 04:45:11 +0100

    Update navbar to show connected network

--- components/ui/navbar/index.tsx
+++ components/ui/navbar/index.tsx
@@ -4,7 +4,7 @@ import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 import Link from 'next/link'
 import ActiveLink from '../link'
 import { useWeb3 } from '@providers/web3'
-import { useAccount } from '@hooks/web3'
+import { useAccount, useNetwork } from '@hooks/web3'
 import Walletbar from './walletbar'
 
 const navigation = [
@@ -19,9 +19,8 @@ function classNames(...classes: string[]) {
 export default function Navbar() {
 
   const {account} = useAccount();
+  const {network} = useNetwork();
 
-  console.log("is Loading ", account.isLoading);
-  console.log("is Installed ", account.isInstalled);
 
   return (
     <Disclosure as="nav" className="bg-gray-800">
@@ -68,7 +67,14 @@ export default function Navbar() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                
+                <div className="text-gray-300 self-center mr-2">
+                  <span className="inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-purple-100 text-purple-800">
+                    <svg className="-ml-0.5 mr-1.5 h-2 w-2 text-indigo-400" fill="currentColor" viewBox="0 0 8 8">
+                      <circle cx={4} cy={4} r={3} />
+                    </svg>
+                    {network.data}
+                  </span>
+                </div>
                 <Walletbar 
                   isLoading={account.isLoading}
                   isInstalled={account.isInstalled}
