80110c699b89ab07ae1f7b1a6100b53705c7e42c
Uduak Essien <acushla4real@gmail.com>
2022-05-16 15:07:40 +0100

    Mint 2 tokens

---
+++ test/commands.js
@@ -0,0 +1,7 @@
+
+
+const instance = await NftMarket.deployed()
+
+instance.mintToken("https://gateway.pinata.cloud/ipfs/Qmb4aom5xNRE5CBRHZsxCsYSdcmX8zfHXgM7ovZxLp3CqL", "500000000000000000", { value: "25000000000000000", from: accounts[0]})
+
+instance.mintToken("https://gateway.pinata.cloud/ipfs/QmPfoHCpmTmoLf27i7zG7fdZnWXygWYbKrkvrPhRyNeZrM", "300000000000000000", {value: "25000000000000000", from: accounts[0]})
