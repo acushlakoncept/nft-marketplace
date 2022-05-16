b66f7e742ed5d240723ec2ada7b96ef74d85ed5b
Uduak Essien <acushla4real@gmail.com>
2022-05-16 15:31:56 +0100

    Implement coreNFT in useListedNFT hook to get Nft data

--- components/hooks/web3/useListedNfts.ts
+++ components/hooks/web3/useListedNfts.ts
@@ -1,12 +1,13 @@
 import { CryptoHookFactory } from "@_types/hooks";
 import { Nft } from "@_types/nft";
+import { ethers } from "ethers";
 import useSWR from "swr";
 
 type UseListedNftsResponse = {
 
 }
 
-type ListedNftsHookFactory = CryptoHookFactory<any, UseListedNftsResponse>
+type ListedNftsHookFactory = CryptoHookFactory<Nft[], UseListedNftsResponse>
 
 export type UseListedNftsHook = ReturnType<ListedNftsHookFactory>
 
@@ -14,10 +15,25 @@ export const hookFactory: ListedNftsHookFactory = ({contract}) => () => {
   const { data, ...swr} = useSWR(
     contract ? "web3/useListedNfts" : null,  
   async () => {
+    const nfts = [] as Nft[];
     const coreNfts = await contract!.getAllNftsOnSale();
     
-    const nfts = [] as any;
+    for(let i = 0; i < coreNfts.length; i++) {
+      const item = coreNfts[i];
+      const tokenURI = await contract!.tokenURI(item.tokenId);
+      const metaRes = await fetch(tokenURI);
+      const meta = await metaRes.json();
 
+      nfts.push({
+        price: parseFloat(ethers.utils.formatEther(item.price)),
+        tokenId: item.tokenId.toNumber(),
+        creator: item.creator,
+        isListed: item.isListed,
+        meta
+      });
+    }
+
+    debugger;
     return nfts;
   })
 
