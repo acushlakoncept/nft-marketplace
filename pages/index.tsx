8149d63e4d9e10bd4ee47f2dc5f825637d0fb344
Uduak Essien <acushla4real@gmail.com>
2022-05-16 16:08:52 +0100

    Finish profile page using the useOwnNft

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,6 @@ import { useListedNfts } from '@hooks/web3'
 const Home: NextPage = () => {
   const { nfts } = useListedNfts();
   
-  console.log(nfts?.data)
   return (
     <BaseLayout>
       <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
