64c02431469c49f0460650247aceaef3841ad76c
Uduak Essien <acushla4real@gmail.com>
2022-05-16 17:10:01 +0100

    Update UI changes for listing NFT

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -139,11 +139,12 @@ const Profile: NextPage = () => {
                     Download Image
                   </button>
                   <button
+                    disabled={activeNft.isListed}
                     onClick={() => nfts.listNft(activeNft.tokenId, activeNft.price)}
                     type="button"
-                    className="flex-1 ml-3 bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    className="disabled:text-gray-400 disabled:cursor-not-allowed flex-1 ml-3 bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                   >
-                    List NFT
+                    {activeNft.isListed ? 'NFT is Listed' : 'List NFT'}
                   </button>
                 </div>
               </div>
