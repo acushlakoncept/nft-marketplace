176afd8c9922125f996f0eaaff99f481f215a49e
Uduak Essien <acushla4real@gmail.com>
2022-05-16 14:43:48 +0100

    Implement useListedNfts hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,14 @@
 
 import type { NextPage } from 'next'
 import { BaseLayout, NftList } from '@ui'
-import nfts from '../content/meta.json'
 import { NftMeta } from '@_types/nft'
 import { useWeb3 } from '@providers/web3'
+import { useListedNfts } from '@hooks/web3'
 
 const Home: NextPage = () => {
-  const {provider, contract } = useWeb3()
-  console.log(provider)
-  console.log(contract)
-
+  const { nfts } = useListedNfts();
   
-
+  console.log(nfts?.data)
   return (
     <BaseLayout>
       <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
@@ -27,7 +24,7 @@ const Home: NextPage = () => {
               Mint a NFT to get unlimited ownership forever!
             </p>
           </div>
-            <NftList nfts={nfts as NftMeta[]} />
+            <NftList nfts={nfts?.data} />
         </div>
       </div>
     </BaseLayout>
