be03fcf20581eeec8d964d42aa989cb8f58ef2d6
Uduak Essien <acushla4real@gmail.com>
2022-05-20 17:02:18 +0100

    Add ropsten network to truffle config file

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const keys = require("./keys.json");
 
 module.exports = {
   contracts_build_directory: "./public/contracts",
@@ -7,6 +9,18 @@ module.exports = {
      port: 7545,
      network_id: "*",
     },
+    ropsten: {
+      provider: () => 
+        new HDWalletProvider(
+          keys.PRIVATE_KEY,
+          keys.INFURA_ROPSTEN_URL
+        ),
+      network_id: 3,
+      gas: 5500000,
+      gasPrice: 20000000000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+    }
   },
 
   compilers: {
