3f0645e547b4e5847a3a2556f6b65a62da9f2e87
Uduak Essien <acushla4real@gmail.com>
2022-05-20 17:01:47 +0100

    Update nft item layout with creator

--- components/ui/nft/item/index.tsx
+++ components/ui/nft/item/index.tsx
@@ -1,9 +1,14 @@
 import { FunctionComponent } from "react";
-import { Nft } from "../../../../types/nft";
+import { NftMeta, Nft } from "../../../../types/nft";
 
 type NftItemProps = {
   item: Nft;
-  buyNft: (tokenId: number, value: number) => Promise<void>;
+  buyNft: (token: number, value: number) => Promise<void>;
+}
+
+function shortifyAddress(address: string) {
+  return address.substring(0, 6) + "..." + address.substring(address.length - 4);
+  // return `0x****${address.slice(-4)}`
 }
 
 const NftItem: FunctionComponent<NftItemProps> = ({item, buyNft}) => {
@@ -18,9 +23,24 @@ const NftItem: FunctionComponent<NftItemProps> = ({item, buyNft}) => {
       </div>
       <div className="flex-1 bg-white p-6 flex flex-col justify-between">
         <div className="flex-1">
-          <p className="text-sm font-medium text-indigo-600">
-            Creatures NFT
-          </p>
+          <div className="flex justify-between items-center">
+            <div className="flex items-center mt-2">
+              <div>
+                <img
+                  className="inline-block h-9 w-9 rounded-full"
+                  src="/images/default_avatar.png"
+                  alt=""
+                />
+              </div>
+              <div className="ml-3">
+                <p className="text-sm font-medium text-gray-700 group-hover:text-gray-900">Creator</p>
+                <p className="text-xs font-medium text-gray-500 group-hover:text-gray-700">{shortifyAddress(item.creator)}</p>
+              </div>
+            </div>
+            <p className="text-sm font-medium text-indigo-600">
+              Creatures NFT
+            </p>
+          </div>
           <div className="block mt-2">
             <p className="text-xl font-semibold text-gray-900">{item.meta.name}</p>
             <p className="mt-3 mb-3 text-base text-gray-500">{item.meta.description}</p>
@@ -33,21 +53,27 @@ const NftItem: FunctionComponent<NftItemProps> = ({item, buyNft}) => {
               <dd className="order-1 text-xl font-extrabold text-indigo-600">
                 <div className="flex justify-center items-center">
                   {item.price}
-                  <img className="h-6" src="/images/small-eth.webp" alt="eth icon" />
+                  <img className="h-6" src="/images/small-eth.webp" alt="ether icon"/>
                 </div>
               </dd>
             </div>
-            { item.meta.attributes.map(attrib => (
-              <div key={attrib.trait_type} className="flex flex-col px-4 pt-4">
-                <dt className="order-2 text-sm font-medium text-gray-500">{attrib.trait_type}</dt>
-                <dd className="order-1 text-xl font-extrabold text-indigo-600">{attrib.value}</dd>
+            { item.meta.attributes.map(attribute =>
+              <div key={attribute.trait_type} className="flex flex-col px-4 pt-4">
+                <dt className="order-2 text-sm font-medium text-gray-500">
+                  {attribute.trait_type}
+                </dt>
+                <dd className="order-1 text-xl font-extrabold text-indigo-600">
+                  {attribute.value}
+                </dd>
               </div>
-            ))}
+            )}
           </dl>
         </div>
         <div>
           <button
-            onClick={() => buyNft(item.tokenId, item.price)}
+            onClick={() => {
+              buyNft(item.tokenId, item.price);
+            }}
             type="button"
             className="disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none disabled:cursor-not-allowed mr-2 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
@@ -64,5 +90,6 @@ const NftItem: FunctionComponent<NftItemProps> = ({item, buyNft}) => {
     </>
   )
 }
+
 
 export default NftItem
